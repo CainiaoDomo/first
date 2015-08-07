21b2a535bc71624dd36396e853de836f6b3e8fce
CainiaoDomo <847767906@qq.com>
2015-08-07 14:19:33 +0800

    输入了关于重定向的一些命令

--- test.sh
+++ test.sh
@@ -5,3 +5,10 @@ echo "你好 世界"
 #!/bin/bash
 echo "CainiaoDomo"
 
+#!/bin/bash
+#把ls -l的信息通过输出重定向导入ls -l.txt这个文件或者目录中
+ls -l > ls -l.txt
+
+#!/bin/bash
+grep da* 2>grep-erros.txt
+#查找所有da开头的文件通过错误重定向导入grep-erros.txt的这个目录下
